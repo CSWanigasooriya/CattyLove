49abea7380bb5dbb215d5c2912b87a7b3e1860f0
Chamath Wanigasooriya <chamathwanigasooriya@gmail.com>
2022-03-24 09:18:35 +0530

    🐞 fix(api): fix collection

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -12,11 +12,11 @@ const dbo = require("../db/conn");
 const ObjectId = require("mongodb").ObjectId;
 
 
-// This section will help you get a list of all the users.
-recordRoutes.route("/user").get(function (req, res) {
-    let db_connect = dbo.getDb("users");
+// This section will help you get a list of all the records.
+recordRoutes.route("/record").get(function (req, res) {
+    let db_connect = dbo.getDb("employees");
     db_connect
-        .collection("users")
+        .collection("records")
         .find({})
         .toArray(function (err, result) {
             if (err) throw err;
@@ -24,33 +24,33 @@ recordRoutes.route("/user").get(function (req, res) {
         });
 });
 
-// This section will help you get a single user by id
-recordRoutes.route("/users/:id").get(function (req, res) {
+// This section will help you get a single record by id
+recordRoutes.route("/record/:id").get(function (req, res) {
     let db_connect = dbo.getDb();
     let myquery = { _id: ObjectId(req.params.id) };
     db_connect
-        .collection("users")
+        .collection("records")
         .findOne(myquery, function (err, result) {
             if (err) throw err;
             res.json(result);
         });
 });
 
-// This section will help you create a new user.
-recordRoutes.route("/users/add").post(function (req, response) {
+// This section will help you create a new record.
+recordRoutes.route("/record/add").post(function (req, response) {
     let db_connect = dbo.getDb();
     let myobj = {
         name: req.body.person_name,
         position: req.body.person_position,
         level: req.body.person_level,
     };
-    db_connect.collection("users").insertOne(myobj, function (err, res) {
+    db_connect.collection("records").insertOne(myobj, function (err, res) {
         if (err) throw err;
         response.json(res);
     });
 });
 
-// This section will help you update a user by id.
+// This section will help you update a record by id.
 recordRoutes.route("/update/:id").post(function (req, response) {
     let db_connect = dbo.getDb();
     let myquery = { _id: ObjectId(req.params.id) };
@@ -70,7 +70,7 @@ recordRoutes.route("/update/:id").post(function (req, response) {
         });
 });
 
-// This section will help you delete a user
+// This section will help you delete a record
 recordRoutes.route("/:id").delete((req, response) => {
     let db_connect = dbo.getDb();
     let myquery = { _id: ObjectId(req.params.id) };
