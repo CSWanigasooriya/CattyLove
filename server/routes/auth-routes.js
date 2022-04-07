3271ec760b53a87877335ecff262c1a5189c7d29
nadinCodeHat <nadinpethiyagoda4@gmail.com>
2022-04-07 14:09:26 +0530

    feat(auth): validation

--- server/routes/auth-routes.js
+++ server/routes/auth-routes.js
@@ -1,50 +1,54 @@
-
 require("dotenv").config({ path: "./config.env" });
 
 const express = require("express");
 
 const authRoutes = express.Router();
 
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 const responseCodes = require("../models/response-codes");
 
 const User = require("../models/user.model");
 
-
 authRoutes.route("/api/auth/register").post(async (req, res) => {
-    try {
-        const user = await User.create({
-            email: req.body.email,
-            password: req.body.password,
-        });
-        res.status(responseCodes.ok).json(user);
-    }
-    catch (err) {
-        res.json({ status: "error", error: err.message });
-    }
+  try {
+    const user = await User.create({
+      email: req.body.email,
+      password: req.body.password,
+    });
+    res.status(responseCodes.ok).json(user);
+  } catch (err) {
+    res.json({ status: "error", error: err.message });
+  }
 });
 
 authRoutes.route("/api/auth/login").post(async (req, res) => {
-    console.log(req.body);
-    try {
-        const user = await User.findOne({
-            email: req.body.email,
-            password: req.body.password,
+  console.log(req.body);
+  try {
+    const user = await User.findOne({
+      email: req.body.email,
+      password: req.body.password,
+    });
+    if (user) {
+      const token = jwt.sign(
+        {
+          uid: user._id,
+          email: req.body.email,
+          password: req.body.password,
+        },
+        process.env.JWT_SECRET
+      );
+      res.status(responseCodes.ok).json({ uid: user._id, token });
+    } else {
+      res
+        .status(responseCodes.unauthorized)
+        .json({
+          errorMessage: "Incorrect email or password, Please try again.",
         });
-        if (user) {
-            const token = jwt.sign({
-                uid: user._id,
-                email: req.body.email,
-                password: req.body.password,
-            }, process.env.JWT_SECRET);
-            res.status(responseCodes.ok).json({ uid: user._id, token });
-        }
-
     }
-    catch (err) {
-        res.json({ status: "error", error: err.message });
-    }
+  } catch (err) {
+    res.json({ status: "error", error: err.message });
+  }
 });
 
 module.exports = authRoutes;
